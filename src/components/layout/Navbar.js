b81c974c4ca708c297afe24f2a17c08d1c800c4e
jarrod1234 <jarrodmalkovic@outlook.com>
2021-02-26 05:09:56 +0800

    Changed navbar text

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ function Navbar() {
 	return (
 		<nav className='flex justify-center p-4'>
 			<h1 className='text-xl text-indigo-400 font-semibold font-heading'>
-				Firepoll
+				Firepoll: Create real-time polls instantly
 			</h1>
 		</nav>
 	);
